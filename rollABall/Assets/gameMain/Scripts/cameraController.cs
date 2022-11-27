0178770f9f49b2a2ed1dfb3def30fd610a274f61
talhaperi1903 <talhaperi44@gmail.com>
2022-11-27 19:53:27 +0300

    rollingABall
    .gitignore

---
+++ rollABall/Assets/gameMain/Scripts/cameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour 
+{
+	public GameObject player;
+	private Vector3 offSet;
+
+	
+	void Start ()
+	{
+		
+		offSet = transform.position - player.transform.position;
+	}
+
+	
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offSet;
+	}
+}
